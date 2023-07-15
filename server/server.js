c2037a22821be8298feaab81fa0241938f3269e9
dezeude <DavidKOS1738@gmail.com>
2023-07-15 18:46:12 -0400

    Clean up and init

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+console.log('server running')
+
+app.get('/server', (req, res, next) => {
+    res.send("Server Responded")
+})
+
+app.listen(5000) //proxy will redirect to port 5000 from package.json
