f7542eb746076dbdfa9f262fa3c54e31ad330daa
dezeude <DavidKOS1738@gmail.com>
2023-07-15 20:15:20 -0400

    Same as before

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express()
 console.log('server running')
 
 app.get('/server', (req, res, next) => {
-    res.send("Server Responded")
+    res.send("<div>Server Responded</div>")
 })
 
 app.listen(5000) //proxy will redirect to port 5000 from package.json
--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import {HtmlParser} from 'react-html-parser'
+
 export default function App() {
+  
   return (
     <div>
-      
+      Hello! <br/>
+      {HtmlParser(initialReq())}
     </div>
   )
+}
+
+async function initialReq(){
+  const req = await fetch('/server')
+  const text = await req.text()
+  console.log(text)
+  return text
 }
