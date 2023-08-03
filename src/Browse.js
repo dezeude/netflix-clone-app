3245c5f37e84d0aa7f8a6cc918017a8833bae434
dezeude <106162055+dezeude@users.noreply.github.com>
2023-08-03 16:13:22 -0400

    Initial commit

--- src/Browse.js
+++ src/Browse.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react'
+import MovieRow from './MovieRow'
+import Landing from './Landing'
+import Navbar from './Navbar'
+import Popup from './Popup'
+
 export default function Browse() {
+  
   return (
-    <div>
-      Browse Page
+    <>
+    <Popup/>
+
+    <Navbar/>
+    
+    <Landing/>
+
+    <div className='main-container'>
+      <MovieRow path={'latest'} title='Latest Releases'/>
+      <MovieRow path={'top_rated'} title='Top Rated'/>
+      <MovieRow path={'now_playing'} title='In Theatres Now'/>
+      <MovieRow path={'popular'} title='Popular'/>
     </div>
+    
+    </>
   )
 }
+
+
